f0b788653785c0d1d57b9554e2479b50af45e177
root <root@bootstrap.us-central1-a.c.smart-poet-258311.internal>
2020-02-27 07:48:10 +0000

    Updates

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y 
+apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y
+apt-get autoremove -y 
 unattended-upgrade -d
